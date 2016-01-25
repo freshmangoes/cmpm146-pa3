6901a2397152b2d04c8fccd01f76f4faf910f08f
Kyle <kylecilia@gmail.com>
2016-01-25 09:25:32 -0800

    Added core files
    Added core files

---
+++ p3_pathfinder.py
@@ -0,0 +1,14 @@
+def find_path(src_pt, dst_pt, mesh):
+    # src_pt is an (x,y) pair
+    # dst_pt is an (x,y) pair
+    # mesh is a mesh data structure
+
+    """
+    Function should return two values
+        path: a list of points like [(x1,y1), (x2,y2)]
+        visited_nodes: a list of boxes explored by the algorithm identified by
+        their bounds (x1,y1, x2,y2) - this is the same as mesh format
+    In p3_interactive.py it will be called like this
+        path, visited_nodes = p3_pathfinder.find_path(src_pt, dst_pt, mesh)
+    """
+    pass
